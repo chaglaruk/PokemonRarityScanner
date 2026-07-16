166c45ebf182f72862d6cecec67bc30679ffc99d
chaglaruk <chaglaruk@gmail.com>
2026-07-16 23:21:11 +0100

    Configure detekt for Kotlin quality analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,31 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.54" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        buildUponDefaultConfig = true
+        allRules = false
+        baseline = rootProject.file("config/detekt/baseline.xml")
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        jvmTarget = "17"
+        exclude("**/build/**", "**/generated/**")
+        reports {
+            html.required.set(true)
+            xml.required.set(true)
+            sarif.required.set(true)
+            md.required.set(true)
+        }
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+        jvmTarget = "17"
+        exclude("**/build/**", "**/generated/**")
+    }
 }
